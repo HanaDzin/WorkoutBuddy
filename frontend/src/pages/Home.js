a031feb0b821acd02424ffb58e031937f3d0559e
HanaDzin <dzinhana@hotmail.com>
2024-10-01 09:29:42 +0200

    fetching data from the frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import React from "react";
 
+import WorkoutDetails from "../components/WorkoutDetails";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  //useEffect so that this function executes when the component first renders
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      //fetching the data from the specified route
+      const response = await fetch("/api/workouts");
+      //parsing the response object into json -> we get an array of Workout objects
+      const json = await response.json();
+      //if the response is ok, update the workouts state
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  });
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
     </div>
   );
 };
