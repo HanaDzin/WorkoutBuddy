f46bb1ba4f060a464f8911c692d9b52b0d2e5c03
HanaDzin <dzinhana@hotmail.com>
2024-10-04 13:01:35 +0200

    adder email & password validation and hashing

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 const UserSchema = new Schema(
@@ -15,5 +17,36 @@ const UserSchema = new Schema(
   },
   { timestamps: true }
 );
+
+//creating a static method on our model (which usually comes with methods such as User.create...)
+UserSchema.statics.signup = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled out.");
+  }
+
+  //validation of email & strength of password
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
+
+  //check to see if email is not already used
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  //hashing the password with bcrypt)
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  //save the user to the database with the hashed password
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", UserSchema);
