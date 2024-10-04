4a7201598e6bed95339f9d27e18f369b1dce6b6d
HanaDzin <dzinhana@hotmail.com>
2024-10-04 13:18:48 +0200

    incorporated jwt tokens for authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,24 @@
 const User = require("../models/UserModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id: _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+};
 
 //login user
 const loginUser = async (req, res) => {
-  res.json({ mssg: "Login User" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    //create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 //signup user
@@ -10,7 +26,11 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+
+    //create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -49,4 +49,22 @@ UserSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+//static login method
+UserSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled out.");
+  }
+
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Incorrect password");
+  }
+  return user;
+};
+
 module.exports = mongoose.model("User", UserSchema);
